80f19f2f0417e41463b038bf140d497529ef2101
Agus Setya <73156742+AgusSetyaNur@users.noreply.github.com>
2025-08-10 11:45:17 +0700

    add some style to "=" button

--- src/Operand.js
+++ src/Operand.js
@@ -5,9 +5,9 @@ function Operand() {
   const { handleOperand, handleOperation } = useCalc();
   return (
     <div className="operand">
-      {operand.map((val) => (
+      {operand.map((val, i) => (
         <div
-          className="operand-item"
+          className={`operand-item operand--item-${i}`}
           onClick={() => (val !== "=" ? handleOperand(val) : handleOperation())}
           key={val}
         >
